f1b94498e1973fb358586c1dce94394b17d68616
lucianofcm <lucweb@gmail.com>
2018-05-30 19:05:06 -0300

    colocando checkbox

--- src/Tarefas/Tarefas.js
+++ src/Tarefas/Tarefas.js
@@ -8,16 +8,17 @@ class Tarefas extends Component {
         super();
         this.state = {
             person:{
-                nome: '',
-                descricao: '',
-                status: null
+                nomeP: '',
+                descricaoP: '',
+                statusP: null
             }
         };
     }
 
     handleChange = e => {
-        const {name, value} = e.target;
-
+        //const {name, value} = e.target;
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        const name = e.target.name;
         this.setState(() => ({
             [name]: value
         }))
@@ -28,10 +29,14 @@ class Tarefas extends Component {
         const {nome, descricao, status} = this.state;
         this.setState(() => ({
             mensagem: `Submitted Foo: ${nome}, Bar: ${descricao}, Baz: ${status}`,
-            person:{nome:nome,descricao:descricao,status:status}
+            person:{nomeP:nome,descricaoP:descricao,statusP:status}
         }))
-        let refa = rootRef.collection('usuarios').doc('teste2');
-        rootRef.collection('usuarios').doc('tarefa5').set(this.state.person);
+        //let refa = rootRef.collection('usuarios').doc('teste2');
+        rootRef.collection('tarefas').doc(nome).set({
+            nome:nome,
+            descricao:descricao,
+            status:status
+        });
         //alert(this.state.person.nome+" "+this.state.person.descricao+" "+this.state.person.status)
     }
 
@@ -42,14 +47,14 @@ class Tarefas extends Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    Foo:
+                    Nome da tarefa:
                     <input onChange={this.handleChange} name="nome" value={nome} />
                     <br />
-                    Bar:
+                    Descrição:
                     <input onChange={this.handleChange} name="descricao" value={descricao} />
                     <br />
-                    Baz:
-                    <input onChange={this.handleChange} name="status" value={status} />
+                    Realizada:
+                    <input  type="checkbox" onChange={this.handleChange} name="status" value={status} />
                     <br />
                     <input type="submit" value="submit" />
                 </form>
