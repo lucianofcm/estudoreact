41373c1059fc3432dc4df4e47dfccabf10a515e7
lucianofcm <lucweb@gmail.com>
2018-05-30 19:49:49 -0300

    alterações diversas

--- src/Tarefas/Tarefas.js
+++ src/Tarefas/Tarefas.js
@@ -1,68 +1,100 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import './Tarefas.css';
-import rootRef from '../Firebase'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./Tarefas.css";
+import rootRef from "../Firebase";
 
 class Tarefas extends Component {
-    constructor() {
-        super();
-        this.state = {
-            person:{
-                nomeP: '',
-                descricaoP: '',
-                statusP: null
-            }
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        nomeP: "",
+        descricaoP: "",
+        statusP: null
+      }
+    };
+  }
 
-    handleChange = e => {
-        //const {name, value} = e.target;
-        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-        const name = e.target.name;
-        this.setState(() => ({
-            [name]: value
-        }))
-    }
+  handleChange = e => {
+    //const {name, value} = e.target;
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const name = e.target.name;
+    this.setState(() => ({
+      [name]: value
+    }));
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const {nome, descricao, status} = this.state;
-        this.setState(() => ({
-            mensagem: `Submitted Foo: ${nome}, Bar: ${descricao}, Baz: ${status}`,
-            person:{nomeP:nome,descricaoP:descricao,statusP:status}
-        }))
-        //let refa = rootRef.collection('usuarios').doc('teste2');
-        rootRef.collection('tarefas').doc(nome).set({
-            nome:nome,
-            descricao:descricao,
-            status:status
-        });
-        //alert(this.state.person.nome+" "+this.state.person.descricao+" "+this.state.person.status)
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    const { nome, descricao, status } = this.state;
+    this.setState(() => ({
+      mensagem: `Tarefa incluída com sucesso ! Nome: ${nome}, Descrição: ${descricao}, Status: ${
+        status ? "Realizada" : "Não realizada"
+      }`
+    }));
+    //let refa = rootRef.collection('usuarios').doc('teste2');
+    rootRef
+      .collection("tarefas")
+      .doc(nome)
+      .set({
+        nome: nome,
+        descricao: descricao,
+        status: status
+      });
+  };
+  divStyleForm = {
+    width: "20rem",
+    "text-align": "center"
+    /*   -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px; */
+  };
+  divStyleCard ={
+    width: '10%',
+    margin: '0 auto'
+  }
 
-    render() {
+  render() {
+    const { nome, descricao, status, mensagem } = this.state;
 
-        const {nome, descricao,status,mensagem} = this.state;
-
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    Nome da tarefa:
-                    <input onChange={this.handleChange} name="nome" value={nome} />
-                    <br />
-                    Descrição:
-                    <input onChange={this.handleChange} name="descricao" value={descricao} />
-                    <br />
-                    Realizada:
-                    <input  type="checkbox" onChange={this.handleChange} name="status" value={status} />
-                    <br />
-                    <input type="submit" value="submit" />
-                </form>
-                {mensagem && <div>Teste {mensagem}</div>}
+    return (
+      <div style={this.divStyleCard}>
+        <div class="card" style={this.divStyleForm}>
+          <form onSubmit={this.handleSubmit}>
+            <div class="form-group">
+              <label for="nome">Nome da tarefa:</label>
+              <input
+                class="form-control"
+                onChange={this.handleChange}
+                name="nome"
+                value={nome}
+              />
             </div>
-        );
-    }
+            <div class="form-group">
+              <label for="descricao">Descrição:</label>
+            <input class="form-control"
+              onChange={this.handleChange}
+              name="descricao"
+              value={descricao}
+            />
+            </div>
+            <br />
+            Realizada:
+            <input
+              type="checkbox"
+              onChange={this.handleChange}
+              name="status"
+              value={status}
+            />
+            <br />
+            <input type="submit" value="submit" />
+          </form>
+          {mensagem && <div>Teste {mensagem}</div>}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Tarefas;
